9e0c7026dd08255eac2e41baa2bf1453f5e99533
Parveen-Rj <165608618+Parveen-Rj@users.noreply.github.com>
2024-09-18 11:07:25 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,10 @@
+
+import Header from "./components/Header";
+export default function Home() {
+  return (
+    <div style={{display: 'flex',justifyContent: 'center',alignItems:'center',height:'100vh'}}>
+      <h1 style={{fontSize: '72px',color:'red',textShadow: '4px 4px 10px gray'}}>Hello World</h1>
+    </div>
+
+  );
+}
